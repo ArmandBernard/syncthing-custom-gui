4e647e57e7a5a5182511228f68b3478eec289413
Armand <armandbernard1995@gmail.com>
2026-07-03 20:39:29 +0100

    Create basic timespan component

---
+++ src/components/TimeSpan.tsx
@@ -0,0 +1,19 @@
+import dayJs from '../lib/dayJs'
+
+export function TimeSpan({ seconds }: { seconds: number }) {
+  const duration = dayJs.duration(seconds, 'seconds')
+
+  let durationStringSections = []
+
+  if (duration.days() > 0) {
+    durationStringSections.push(`${duration.days()}d`)
+  }
+  if (duration.hours() > 0) {
+    durationStringSections.push(`${duration.hours()}h`)
+  }
+  if (duration.minutes() > 0) {
+    durationStringSections.push(`${duration.minutes()}m`)
+  }
+
+  return durationStringSections.join(' ')
+}
