04b5e6f246d00912ae889396624b57a3bcb38725
Armand <armandbernard1995@gmail.com>
2026-07-03 21:02:17 +0100

    Create component to show other devices

--- src/Dashboard.tsx
+++ src/Dashboard.tsx
@@ -1,10 +1,12 @@
 import { DeviceInfo } from './DeviceInfo.tsx'
+import { ConnectedDevices } from './components/ConnectedDevices.tsx'
 
 export function Dashboard() {
   return (
     <div className="w-xl self-center pt-4 flex flex-col gap-4">
       <h1 className="text-2xl">Dashboard</h1>
       <DeviceInfo />
+      <ConnectedDevices />
     </div>
   )
 }
---
+++ src/components/ConnectedDevices.tsx
@@ -0,0 +1,50 @@
+import { CircularProgress } from './ui/Progress.tsx'
+import { useSyncthingQuery } from '../hooks/useSyncthingQuery.ts'
+import type { Connection } from '../lib/syncthing/types/system.ts'
+import { Card } from './ui/Card.tsx'
+import { ByteSize } from './ByteSize.tsx'
+
+export function ConnectedDevices() {
+  const { data: connections, isLoading: connectionsAreLoading } = useSyncthingQuery(
+    'GET /system/connections',
+    { refetchInterval: 5000 },
+  )
+
+  if (connectionsAreLoading) {
+    return <CircularProgress aria-label="Loading" />
+  }
+
+  return (
+    <ul className="flex flex-col gap-2">
+      {Object.entries(connections?.connections ?? {}).map(([id, connection]) => (
+        <li key={id}>
+          <Card>
+            <div>
+              (Name) <ConnectionStatus connection={connection} />
+            </div>
+            <ul>
+              <li>
+                Upload: <ByteSize bytes={connection.outBytesTotal} />
+              </li>
+              <li>
+                Download: <ByteSize bytes={connection.inBytesTotal} />
+              </li>
+            </ul>
+          </Card>
+        </li>
+      ))}
+    </ul>
+  )
+}
+
+function ConnectionStatus({ connection }: { connection: Connection }) {
+  if (!connection.connected) {
+    return <div className="text-error">Disconnected</div>
+  }
+  if (connection.paused) {
+    return <div className="text-amber-400">Paused</div>
+  }
+  if (connection.connected) {
+    return <div className="text-success">Connected</div>
+  }
+}
