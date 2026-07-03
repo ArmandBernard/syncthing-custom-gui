ddf75eb229bf6932b5431cfbeb3d0ad631eefe30
Armand <armandbernard1995@gmail.com>
2026-07-03 20:58:04 +0100

    Show transfer amount

--- src/DeviceInfo.tsx
+++ src/DeviceInfo.tsx
@@ -2,14 +2,26 @@ import { CircularProgress } from './components/ui/Progress.tsx'
 import { useSyncthingQuery } from './hooks/useSyncthingQuery.ts'
 import { Card } from './components/ui/Card.tsx'
 import { TimeSpan } from './components/TimeSpan.tsx'
+import { ByteSize } from './components/ByteSize.tsx'
 
 export function DeviceInfo() {
   const { data: status, isLoading: statusIsLoading } = useSyncthingQuery('GET /system/status', {
     refetchInterval: 10000,
   })
   const { data: config, isLoading: configIsLoading } = useSyncthingQuery('GET /config')
+  const { data: connections, isLoading: connectionsAreLoading } = useSyncthingQuery(
+    'GET /system/connections',
+    { refetchInterval: 5000 },
+  )
 
-  if (statusIsLoading || !status || configIsLoading || !config) {
+  if (
+    statusIsLoading ||
+    !status ||
+    configIsLoading ||
+    !config ||
+    connectionsAreLoading ||
+    !connections
+  ) {
     return <CircularProgress aria-label="Loading" />
   }
 
@@ -23,6 +35,12 @@ export function DeviceInfo() {
         <li>Name: {myDeviceConfigInfo.name}</li>
         <li>
           Uptime: <TimeSpan seconds={status.uptime} />
+        </li>
+        <li>
+          Download: <ByteSize bytes={connections.total.inBytesTotal} />
+        </li>
+        <li>
+          Upload: <ByteSize bytes={connections.total.outBytesTotal} />
         </li>
       </ul>
     </Card>
