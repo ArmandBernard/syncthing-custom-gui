81f8cf0795bd3a7892b754f7e10d22bc20dba24a
Armand <armandbernard1995@gmail.com>
2026-07-03 19:31:17 +0100

    Add a border around the header on light theme

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,7 +3,10 @@ import { StatusIndicator } from './StatusIndicator.tsx'
 
 export function Header() {
   return (
-    <header className="flex justify-between p-2 gap-2 bg-surface-container-highest">
+    <header
+      className={`flex justify-between gap-2 p-2
+        border-b dark:border-b-none border-outline-variant bg-surface-container-highest shadow-md`}
+    >
       <div className="text-xl flex items-center">syncthing-custom-gui</div>
       <div className="flex">
         <StatusIndicator />
