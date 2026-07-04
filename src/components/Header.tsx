45bb244b3c084dbe85b8b9714e4ba7887925ced8
Armand <armandbernard1995@gmail.com>
2026-07-04 13:46:12 +0100

    Show the icon in the header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,11 +1,12 @@
 import { AppMenu } from './AppMenu.tsx'
 import { StatusIndicator } from './StatusIndicator.tsx'
+import { SyncthingIconDark } from './SyncthingIconDark.tsx'
 
 export function Header() {
   return (
     <header className="flex items-center justify-between gap-2 p-2 bg-surface-high shadow-sm">
       <div className="flex items-center gap-2">
-        <img alt="" aria-hidden className="h-8 w-8" src="/favicon.svg" />
+        <SyncthingIconDark className="h-8 w-8" />
         <div className="text-xl pb-1">syncthing-custom-gui</div>
       </div>
       <div className="flex gap-2">
