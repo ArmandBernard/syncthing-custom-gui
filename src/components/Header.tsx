e7f37965b80823e3bd91a1635a5c3dbdc7b677d6
Armand <armandbernard1995@gmail.com>
2026-07-04 13:33:26 +0100

    Add icon to header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,8 +3,11 @@ import { StatusIndicator } from './StatusIndicator.tsx'
 
 export function Header() {
   return (
-    <header className="flex justify-between gap-2 p-2 bg-surface-high shadow-sm">
-      <div className="text-xl flex items-center">syncthing-custom-gui</div>
+    <header className="flex items-center justify-between gap-2 p-2 bg-surface-high shadow-sm">
+      <div className="flex items-center gap-2">
+        <img alt="" aria-hidden className="h-8 w-8" src="/favicon.svg" />
+        <div className="text-xl pb-1">syncthing-custom-gui</div>
+      </div>
       <div className="flex gap-2">
         <StatusIndicator />
         <AppMenu />
