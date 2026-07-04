91ef1d9c892c910a1505b8c073e30cecf7572994
Armand <armandbernard1995@gmail.com>
2026-07-04 21:18:35 +0100

    Add to dashboard

--- src/Dashboard.tsx
+++ src/Dashboard.tsx
@@ -1,12 +1,20 @@
 import { ThisDevice } from './ThisDevice.tsx'
 import { RemoteDevices } from './components/RemoteDevices.tsx'
+import { Folders } from './Folders.tsx'
 
 export function Dashboard() {
   return (
-    <div className="w-lg self-center pt-4 flex flex-col gap-4">
+    <div className="self-center pt-4 flex flex-col gap-4">
       <h1 className="text-2xl">Dashboard</h1>
-      <ThisDevice />
-      <RemoteDevices />
+      <div className="flex flex-col lg:flex-row gap-x-8 gap-y-4">
+        <div className="w-md">
+          <Folders />
+        </div>
+        <div className="w-md flex flex-col gap-4">
+          <ThisDevice />
+          <RemoteDevices />
+        </div>
+      </div>
     </div>
   )
 }
