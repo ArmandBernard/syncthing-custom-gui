8c1d6d304bf9d2773ec54316d61347ad43012259
Armand <armandbernard1995@gmail.com>
2026-07-11 08:49:19 +0100

    Group folders

--- src/Folders.tsx
+++ src/Folders.tsx
@@ -9,16 +9,23 @@ export function Folders() {
     return <CircularProgressCentred name="folders" />
   }
 
+  const grouped = Object.groupBy(config.folders, (folder) => folder.group)
+
   return (
     <div className="flex flex-col gap-4">
       <h2 className="text-2xl">Folders</h2>
-      <ul className="flex flex-col gap-2">
-        {config.folders.map((folder) => (
-          <li key={folder.id}>
-            <Folder folder={folder} devices={config.devices} />
-          </li>
-        ))}
-      </ul>
+      {Object.entries(grouped).map(([group, value]) => (
+        <>
+          {group && <h3 className="text-xl">{group}</h3>}
+          <ul className="flex flex-col gap-2">
+            {value!.map((folder) => (
+              <li key={folder.id}>
+                <Folder folder={folder} devices={config.devices} />
+              </li>
+            ))}
+          </ul>
+        </>
+      ))}
     </div>
   )
 }
