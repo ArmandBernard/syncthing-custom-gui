dc4171f50266d04cf5af17defe18ac791f55fcff
Armand <armandbernard1995@gmail.com>
2026-07-11 11:17:14 +0100

    sort RemoteDevices too

--- src/RemoteDevices.tsx
+++ src/RemoteDevices.tsx
@@ -21,30 +21,33 @@ export function RemoteDevices() {
   return (
     <div className="flex flex-col gap-4">
       <h2 className="text-2xl">Remote devices</h2>
-      {Object.entries(grouped).map(([group, value]) => (
-        <>
-          {group && <h3 className="text-xl">{group}</h3>}
-          <ul className="flex flex-col gap-2">
-            {value!
-              .toSorted((da, db) => da.name.localeCompare(db.name))
-              .map((device) => {
-                const connection: Connection | undefined = connections.connections[device.deviceID]
-                const deviceStats = stats[device.deviceID]
+      {Object.entries(grouped)
+        .toSorted((a, b) => a[0].localeCompare(b[0]))
+        .map(([group, value]) => (
+          <>
+            {group && <h3 className="text-xl">{group}</h3>}
+            <ul className="flex flex-col gap-2">
+              {value!
+                .toSorted((da, db) => da.name.localeCompare(db.name))
+                .map((device) => {
+                  const connection: Connection | undefined =
+                    connections.connections[device.deviceID]
+                  const deviceStats = stats[device.deviceID]
 
-                // typically happens if this connection is you
-                if (!connection) {
-                  return null
-                }
+                  // typically happens if this connection is you
+                  if (!connection) {
+                    return null
+                  }
 
-                return (
-                  <li key={device.deviceID}>
-                    <Device device={device} connection={connection} stats={deviceStats} />
-                  </li>
-                )
-              })}
-          </ul>
-        </>
-      ))}
+                  return (
+                    <li key={device.deviceID}>
+                      <Device device={device} connection={connection} stats={deviceStats} />
+                    </li>
+                  )
+                })}
+            </ul>
+          </>
+        ))}
     </div>
   )
 }
