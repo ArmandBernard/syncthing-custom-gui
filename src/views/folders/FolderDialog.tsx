d3a7bb7c1efe3768cfd5c9aaaae40692c08721ad
Armand <armandbernard1995@gmail.com>
2026-07-18 12:31:37 +0100

    Show sharing status in FolderDialog

--- src/views/folders/FolderDialog.tsx
+++ src/views/folders/FolderDialog.tsx
@@ -1,7 +1,11 @@
 import { Dialog } from '@components/ui/Dialog.tsx'
 import { TextField } from '@components/ui/TextField.tsx'
 import { useState } from 'react'
-import type { FolderConfiguration, FolderDeviceConfiguration } from '@lib/syncthing/types/config'
+import type {
+  DeviceConfiguration,
+  FolderConfiguration,
+  FolderDeviceConfiguration,
+} from '@lib/syncthing/types/config'
 import { Button } from '@components/ui/Button.tsx'
 import { useSyncthingMutation } from '@hooks/useSyncthingMutation.ts'
 import { useSyncthingQuery } from '@hooks/useSyncthingQuery.ts'
@@ -16,6 +20,8 @@ import { Checkbox } from '@components/ui/Checkbox.tsx'
 import { useDeviceID } from '@context/device-id/useDeviceID.ts'
 import { CircularProgressCentred } from '@components/CircularProgressCentred.tsx'
 import { ErrorAlert } from '@components/ui/ErrorAlert.tsx'
+import { useConnections } from '@context/connections/useConnections.ts'
+import getSharedFolderStatus, { type SharedFolderStatus } from '@lib/getSharedFolderStatus.ts'
 
 type FolderDialogTabs = 'general' | 'sharing'
 
@@ -244,16 +250,6 @@ function SharingForm({
     return <CircularProgressCentred name="devices" />
   }
 
-  function handleShareDevice(device: FolderDeviceConfiguration) {
-    onUpdateConfiguration({ devices: [...effectiveConfig.devices, device] })
-  }
-
-  function handleUnshareDevice(deviceId: DeviceID) {
-    onUpdateConfiguration({
-      devices: effectiveConfig.devices.filter((d) => d.deviceID !== deviceId),
-    })
-  }
-
   const devices = devicesIncludingMe.filter((d) => d.deviceID !== myDeviceId)
   const sharedDeviceIds = new Set<DeviceID>(effectiveConfig.devices.map((d) => d.deviceID))
 
@@ -265,27 +261,95 @@ function SharingForm({
           const shared = sharedDeviceIds.has(device.deviceID)
 
           return (
-            <li key={device.deviceID} className="flex gap-4 justify-between items-center">
-              <Checkbox
-                label={device.name}
-                className="flex-1"
-                checked={shared}
-                onChange={(event) => {
-                  if (event.currentTarget.checked) {
-                    handleShareDevice({
-                      deviceID: device.deviceID,
-                      encryptionPassword: '',
-                      introducedBy: '',
-                    })
-                  } else {
-                    handleUnshareDevice(device.deviceID)
-                  }
-                }}
-              />
-            </li>
+            <SharingItem
+              key={device.deviceID}
+              device={device}
+              shared={shared}
+              effectiveConfig={effectiveConfig}
+              onUpdateConfiguration={onUpdateConfiguration}
+            />
           )
         })}
       </ul>
     </div>
   )
+}
+
+function SharingItem({
+  device,
+  shared,
+  effectiveConfig,
+  onUpdateConfiguration,
+}: {
+  device: DeviceConfiguration
+  shared: boolean
+  effectiveConfig: FolderConfiguration
+  onUpdateConfiguration: (configUpdates: Partial<FolderConfiguration>) => void
+}) {
+  const { data: completion } = useSyncthingQuery('GET /db/completion', {
+    enabled: shared,
+    query: { device: device.deviceID, folder: effectiveConfig.id },
+  })
+  const connections = useConnections()
+  const connection = connections?.connections[device.deviceID]
+
+  function handleShareDevice(device: FolderDeviceConfiguration) {
+    onUpdateConfiguration({ devices: [...effectiveConfig.devices, device] })
+  }
+
+  function handleUnshareDevice() {
+    onUpdateConfiguration({
+      devices: effectiveConfig.devices.filter((d) => d.deviceID !== device.deviceID),
+    })
+  }
+
+  return (
+    <li key={device.deviceID} className="flex gap-4 justify-between items-center">
+      <Checkbox
+        label={
+          shared ? (
+            <div className="flex gap-4 justify-between">
+              <span>{device.name}</span>
+              <FolderStatusText
+                folderStatus={getSharedFolderStatus({
+                  connection: connection,
+                  completion,
+                })}
+              />
+            </div>
+          ) : (
+            device.name
+          )
+        }
+        className="flex-1"
+        checked={shared}
+        onChange={(event) => {
+          if (event.currentTarget.checked) {
+            handleShareDevice({
+              deviceID: device.deviceID,
+              encryptionPassword: '',
+              introducedBy: '',
+            })
+          } else {
+            handleUnshareDevice()
+          }
+        }}
+      />
+    </li>
+  )
+}
+
+function FolderStatusText({ folderStatus }: { folderStatus: SharedFolderStatus }) {
+  switch (folderStatus) {
+    case 'accepted':
+      return <span className="text-on-surface-connected">Accepted</span>
+    case 'ignored':
+      return <span className="text-error">Ignored</span>
+    case 'not-accepted':
+      return <span className="text-on-surface-variant">Not accepted</span>
+    case 'disconnected':
+      return <span className="text-on-surface-disconnected">Disconnected</span>
+    default:
+      return null
+  }
 }
