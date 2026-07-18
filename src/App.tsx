ce6925f30aabab67958669202c5b3b8fcc4b60ba
Armand <armandbernard1995@gmail.com>
2026-07-18 15:11:00 +0100

    Set background color in html to prevent flashing

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,7 @@ const Dashboard = lazy(() => import('./Dashboard.tsx'))
 
 function App() {
   return (
-    <div className="flex flex-col min-h-screen">
+    <div className="flex flex-col min-h-screen bg-surface-low">
       <Header />
 
       <main className="flex flex-1 flex-col">
