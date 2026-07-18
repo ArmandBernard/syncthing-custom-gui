06941df7a019ea987f870d791a6cac82c956e684
Armand <armandbernard1995@gmail.com>
2026-07-18 10:28:51 +0100

    hook up share removal

--- src/views/folders/AcceptFolderAlerts.tsx
+++ src/views/folders/AcceptFolderAlerts.tsx
@@ -4,6 +4,9 @@ import type { DeviceConfiguration } from '@lib/syncthing/types/config'
 import { getEnumEntries } from '@lib/getEnumEntries.ts'
 import { RelativeTime } from '@components/RelativeTime.tsx'
 import { useSyncthingQuery } from '@hooks/useSyncthingQuery.ts'
+import { Button } from '@components/ui/Button.tsx'
+import { useSyncthingMutation } from '@hooks/useSyncthingMutation.ts'
+import { useSyncthingInvalidate } from '@hooks/useSyncthingInvalidate.ts'
 
 export function AcceptFolderAlerts({ devices }: { devices: DeviceConfiguration[] }) {
   const { data: pendingFolders } = useSyncthingQuery('GET /cluster/pending/folders')
@@ -39,15 +42,23 @@ function AcceptFolderAlert({
   folder: PendingFolder
   deviceDictionary: Map<DeviceID, DeviceConfiguration>
 }) {
+  const { mutateAsync: removeOffer, isPending: removeOfferIsPending } = useSyncthingMutation(
+    'DELETE /cluster/pending/folders',
+  )
+  const invalidate = useSyncthingInvalidate('GET /cluster/pending/folders')
   const offers = getEnumEntries(folder.offeredBy)
-
   const offerPair1 = offers[0]
   const [offer1Device, offer1Offer] = offerPair1
+
+  async function handleOnRemoveClick() {
+    await removeOffer({ query: { folder: folderID } })
+    await invalidate()
+  }
 
   return (
     <div
       role="alert"
-      className="flex flex-col gap-4 bg-primary-container text-on-primary rounded-md p-4"
+      className="flex flex-col gap-4 bg-primary-container text-on-primary-container rounded-md p-4"
     >
       <div className="flex justify-between items-baseline gap-4 text-lg">
         <div>New folder</div>
@@ -58,6 +69,12 @@ function AcceptFolderAlert({
       <div className="text-sm">
         {deviceDictionary.get(offer1Device)?.name} wants to share folder "{offer1Offer.label}" (
         {folderID})
+      </div>
+      <div className="flex gap-2 justify-end">
+        <Button variant="filled">Add</Button>
+        <Button variant="tonal" disabled={removeOfferIsPending} onClick={handleOnRemoveClick}>
+          Ignore
+        </Button>
       </div>
     </div>
   )
